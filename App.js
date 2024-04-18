7291fdee2c622361d7f402027b009a3a91b078ee
Infinite-Concept <ifenowoifesegun@gmail.com>
2024-04-18 15:25:20 -0400

    added login & register interface so create model for database

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StackNavigator />
   );
 }
 
--- api/index.js
+++ api/index.js
@@ -12,3 +12,36 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 const jwt = require("jsonwebtoken")
+
+mongoose.connect("mongodb+srv://ifenowoifesegun:ifenowoifesegun@cluster0.ywxmand.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("connected to MongoDb");
+}).catch((err) => {
+    console.log("Error connecting to MongoDb");
+})
+
+app.listen(port, () => {
+    console.log("server is running on port 3000");
+})
+
+const User = require("./models/user")
+const passwordost = require("./models/post")
+
+// endpoint to register a user in the backend
+app.post("/register", async(req, res) => {
+    try{
+
+        const {name, email, password} = req.body
+ 
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "Email already registered" })
+        }
+
+    }catch(error){
+        console.log("error registering user", error);
+        res.status(500).json({message: "error registering user"});
+    }
+})
